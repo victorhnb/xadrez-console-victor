c7556f166d2595d42476f5e7b674cfa1967892f8
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-06-23 01:19:52 -0300

    Projeto iniciado

---
+++ xadrez-console-victor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xadrez_console_victor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
