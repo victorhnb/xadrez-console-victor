573a17012cec0435f800407256690958abf5ee87
Victor Hugo <victor_nascimento1996@hotmail.com>
2022-06-23 01:45:27 -0300

    Comecando a implementar o tabuleiro

--- xadrez-console-victor/Program.cs
+++ xadrez-console-victor/Program.cs
@@ -7,11 +7,9 @@ namespace xadrez_console_victor
     {
         static void Main(string[] args)
         {
-            Posicao P;
 
-            P = new Posicao(3, 4);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine("Posição: " + P);
         }
     }
 }
